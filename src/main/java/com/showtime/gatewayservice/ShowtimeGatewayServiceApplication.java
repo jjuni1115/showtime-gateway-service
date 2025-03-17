82e4d6106f7f2d8c6139bfb521726f7592abaef8
jjuni1115 <junyege@naver.com>
2025-03-17 17:39:42 +0900

    initialize showtime-gateway-service with Gradle and Spring Boot setup

---
+++ src/main/java/com/showtime/gatewayservice/ShowtimeGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.showtime.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowtimeGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowtimeGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/showtime/gatewayservice/ShowtimeGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showtime.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowtimeGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
